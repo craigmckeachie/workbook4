09228ce306dd45a94b66173ad61bfcabc20fa98c
Craig McKeachie <craigdmckeachie@gmail.com>
2022-10-11 15:54:49 -0400

    array examples

---
+++ demos/arrays.js
@@ -0,0 +1,16 @@
+let names = [
+  "Tarik",
+  "India",
+  "Brian",
+  "Samuel",
+  "Millena",
+  "Gabriel",
+  "Ngoc",
+  "LC",
+  "Jarel",
+];
+
+for (let index = 0; index < names.length; index++) {
+  const element = names[index];
+  console.log(element);
+}
