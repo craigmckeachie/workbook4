f89034237e04943ef259eda9275c1f8e23ca2bab
Craig McKeachie <craigdmckeachie@gmail.com>
2022-10-11 14:39:22 -0400

    loop demos

---
+++ demos/loops.js
@@ -0,0 +1,32 @@
+//while loop
+// let count = 1;
+
+// while (count <= 10) {
+//   console.log(count);
+//   count = count + 1;
+// }
+
+//do while
+// let count = 1;
+
+// do {
+//   console.log(count);
+//   count = count + 1;
+// } while (count <= 10);
+
+//for loop
+// for (let counter = 1; counter <= 10; counter = counter + 1) {
+//   console.log(counter);
+// }
+
+//for loop using ++ operator
+// for (let count = 1; count <= 10; count++) {
+//   console.log(count);
+// }
+
+
+for (let count = 0; count < 1000; count++) {
+    console.log(count);
+}
+
+
