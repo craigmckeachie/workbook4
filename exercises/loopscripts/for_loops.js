82ab2f24de302689d780bda073a6ea7605eaf4d3
Craig McKeachie <craigdmckeachie@gmail.com>
2022-10-11 15:03:16 -0400

    while loops

---
+++ exercises/loopscripts/for_loops.js
@@ -0,0 +1,8 @@
+//initialize counter
+//check counter using a conditional
+// increment (change) counter
+for (let loop = 1; loop < 8; loop++) {
+  console.log("I love loops");
+}
+
+
---
+++ exercises/loopscripts/while_loops.js
@@ -0,0 +1,9 @@
+//initialize counter
+let loop = 1;
+
+//check counter using a conditional
+while (loop < 8) {
+  console.log("I love loops");
+  // increment (change) counter
+  loop++;
+}
