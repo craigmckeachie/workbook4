b8017abfe28b4ccd5b889a3be318a20c2b750ffe
Craig McKeachie <craigdmckeachie@gmail.com>
2022-10-12 10:11:37 -0400

    average scores

--- exercises/arrayscripts/avg_scores.js
+++ exercises/arrayscripts/avg_scores.js
@@ -1,19 +1,16 @@
 let myScores = [92, 98, 84, 76, 89, 99, 100];
 let yourScores = [82, 98, 94, 88, 92, 100, 100];
 
-
 //part1 = where to start, creating count variable and initializing it (start)
 //part 2 = conditional, comparision that returns true or false, should loop continue (end)
 //part3 = how to count
 function getAverage(scores) {
-    for (let i = 0; i < scores.length; i++){
-        console.log(scores[i]);
-    }
+  let total = 0;
+  for (let i = 0; i < scores.length; i++) {
+    total = total + scores[i];
+  }
+  return total / scores.length;
 }
 
-getAverage(myScores);
-
-
-
-
-
+let averageScore = getAverage(myScores);
+console.log(averageScore.toFixed(2));
