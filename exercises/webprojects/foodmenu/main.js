af897d783ff056feb61c93aa09024a884b1f0b2e
Craig McKeachie <craigdmckeachie@gmail.com>
2022-10-14 10:03:57 -0400

    mini project working

---
+++ exercises/webprojects/foodmenu/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let menu = {
+  drinks: ["Water", "Tea", "Sweet Tea", "Coke", "Dr. Pepper", "Sprite"],
+  entrees: [
+    "Hamburger w/ Fries",
+    "Grilled Cheese w/ Tater Tots",
+    "Grilled Chicken w/ Veggies",
+    "Chicken Fried Steak w/ Mashed Potatoes",
+    "Fried Shrimp w/ Coleslaw",
+    "Veggie Plate",
+  ],
+  desserts: ["Cheesecake", "Chocolate Cake", "Snickerdoodle Cookie"],
+};
+
+const categoryList = document.getElementById("categoryList");
+const itemList = document.getElementById("itemList");
+
+function loadItemList() {
+  //clear all list items
+  itemList.options.length = 0;
+  const selectedValue = categoryList.value;
+  for (const item of menu[selectedValue]) {
+    addOptionToList(item, item, itemList);
+  }
+}
+
+function addOptionToList(text, value, list) {
+  let option = new Option(text, value);
+  list.appendChild(option);
+}
+
+window.onload = function () {};
